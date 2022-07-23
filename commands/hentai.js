fafa02c4ef950fa5d29099159a8401c2d4bb611b
Dinhero21 <robloxdinhero21@gmail.com>
2022-07-23 04:06:09 -0300

    Hentai command

---
+++ commands/hentai.js
@@ -0,0 +1,45 @@
+const { resize } = require('../util/image')
+const axios = require('axios')
+const sharp = require('sharp')
+const { RandomPHUB } = require('discord-phub')
+
+const nsfw = new RandomPHUB(true)
+
+module.exports = {
+  name: 'hentai',
+  async minecraft (handler) {
+    let category = handler.args.join(' ')
+
+    if (category.length === 0) category = '3d-porn'
+
+    const { url } = nsfw.getRandomInCategory(category, 'png')
+
+    const result = await axios.get(url, {
+      responseType: 'arraybuffer'
+    })
+
+    const raw = result.data
+
+    const metadata = await sharp(raw)
+      .metadata()
+
+    const { width, height } = resize(metadata.width, metadata.height)
+
+    const { data, info } = await sharp(raw)
+      .resize({ fit: 'fill', kernel: 'nearest', width, height })
+      .raw()
+      .toBuffer({ resolveWithObject: true })
+
+    handler.bot.draw(data, info)
+
+    handler.sendMessage({
+      text: 'Click here to see the original image',
+      color: 'yellow',
+      bold: true,
+      clickEvent: {
+        action: 'open_url',
+        value: url
+      }
+    })
+  }
+}
