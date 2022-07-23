d3c4dbc524505c6247893777eaf5420a9be8b576
Dinhero21 <robloxdinhero21@gmail.com>
2022-07-23 08:19:56 -0300

    Exception and Rejection handling

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const config = require('./config.json')
 const { createBots } = require('./minecraft')
 
 createBots(config.servers)
+
+process.on('uncaughtException', error => {
+  console.error('[Uncaught exception]', error)
+})
+
+process.on('unhandledRejection', error => {
+  console.error('[Unhandled rejection]', error)
+})
