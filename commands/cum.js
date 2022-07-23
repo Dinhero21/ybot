9b6166d08467cad185103921fd4fdc241c6f3d7f
Dinhero21 <robloxdinhero21@gmail.com>
2022-07-23 01:42:06 -0300

    Updated cum command to use selectUsername

--- commands/cum.js
+++ commands/cum.js
@@ -1,8 +1,8 @@
-const { UUIDToInt } = require('../util/player')
+const { selectUsername } = require('../util/player')
 
 module.exports = {
   name: 'cum',
   minecraft (handler) {
-    handler.bot.core.run(`minecraft:give @p[nbt={UUID:[I;${UUIDToInt(handler.sender).value.join()}]}] potion{display:{Name:'{"text":"Cum","italic":false}'},CustomPotionColor:16777215}`)
+    handler.bot.core.run(`minecraft:give ${selectUsername(handler.bot, handler.sender)} potion{display:{Name:'{"text":"Cum","italic":false}'},CustomPotionColor:16777215}`)
   }
 }
