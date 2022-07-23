7d9befc04913e23817f3006c4d520899da0d7923
Dinhero21 <robloxdinhero21@gmail.com>
2022-07-23 01:41:55 -0300

    selectUsername function on player util

--- util/player.js
+++ util/player.js
@@ -5,8 +5,14 @@ function parseUsername (username) {
     .replace(/§c/, '')
 }
 
+function selectUsername (bot, username) {
+  const player = bot.players.getPlayer(username)
+
+  return player ? `@p[nbt={UUID:[I;${UUIDToInt(player.UUID).value.join()}]}]` : username
+}
+
 function UUIDToInt (uuid) {
   return nbt.intArray(uuid.replace(/-/g, '').match(/.{8}/g).map(string => Number.parseInt(string, 16)).map(number => number & 0x80000000 ? number - 0xffffffff - 1 : number))
 }
 
-module.exports = { parseUsername, UUIDToInt }
+module.exports = { parseUsername, UUIDToInt, selectUsername }
