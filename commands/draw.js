573b6c3bae31c07b93d8d92190ef72f5cc8a788a
Dinhero21 <robloxdinhero21@gmail.com>
2022-07-23 08:20:10 -0300

    Faster image loading

--- commands/draw.js
+++ commands/draw.js
@@ -8,12 +8,14 @@ module.exports = {
     const name = handler.args.join(' ').replaceAll(/§r/g, '')
     const fullpath = path.join(__dirname, '../images', name)
 
-    const metadata = await sharp(fullpath)
+    const image = await sharp(fullpath)
+
+    const metadata = await image
       .metadata()
 
     const { width, height } = resize(metadata.width, metadata.height)
 
-    const { data, info } = await sharp(fullpath)
+    const { data, info } = await image
       .resize({ fit: 'fill', kernel: 'nearest', width, height })
       .raw()
       .toBuffer({ resolveWithObject: true })
